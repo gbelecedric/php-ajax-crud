0d73a2c46644651762c2283de640debbc2fe8e34
gbelecedric <cedric.gbele@uvci.edu.ci>
2021-02-12 13:15:38 +0000

    go

--- database.php
+++ database.php
@@ -3,10 +3,32 @@
    class Database
    {
        private static $dbHost = "localhost";
-       private static $dbName = "localhost";
+       private static $dbName = "testing";
        private static $dbUsername = "root";
        private static $dbUserpassword = "";
        private static $connection = null;
 
+       public static function connect()
+       {
+           if(self::$connection == null )
+           {
+               try {
+                   self::$connection = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName , self::$dbUsername, self::$dbUserpassword );
+               } 
+               catch(PDOException $e)
+               {
+                   die($e->getMessage());
+               }
+           }
+           return self::$connection;
+       }
+
+       public static function disconnect()
+       {
+           self::$connection = null;
+       }
+
+
    }
+   
 ?>
