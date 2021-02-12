7ff853682b99d23cd30f939e429b3a75e4dd7ce1
gbelecedric <cedric.gbele@uvci.edu.ci>
2021-02-12 13:48:28 +0000

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -9,10 +9,59 @@ $result = $statement->fetchAll();
 $total_row = $statement->rowCount();
 
 $output = '
+    <table class="table table-striped table-bordered">
 
+    
+        <tr>
+            <th> First Name</th>
+            <th > Last Name</th>
+            <th > Edit</th>
+            <th > Delete</th>
+
+        </tr>
+
+   
 
 ';
 
+if($total_row > 0)
+{
+   foreach ($result as $row) {
+       # code...
+$output .= '
 
+    <tr>
+    <td> '.row["fist_name"].'</td>
+    <td> '.row["last_name"].'</td>
+    <td>
+        <button type="button" name="edit" class="btn btn-primary 
+        btn-xs edit" id ="'.$row["id"].'">Edit
+        </button>
+    </td>
+    <td > 
+    <button type="button" name="delete" class="btn btn-danger 
+    btn-xs delete" id ="'.$row["id"].'">Delete
+    </button>
     
+    </td>
+
+    </tr>
+';
+   }
+}
+ else
+ {
+    $output .= '
+    <tr>
+        <td colspan="4" align="center">Data not found </td>
+    </tr>
+    ';
+
+
+ }   
+
+$output .= ' </table>';
+
+echo $output;
+
 ?>
