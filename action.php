37e67c8bb9cff06114c94fca00991b750e18398c
gbelecedric <cedric.gbele@uvci.edu.ci>
2021-02-12 15:36:36 +0000

    Update action.php

--- action.php
+++ action.php
@@ -3,7 +3,16 @@
 require 'database.php';
 $db = Database::connect();
 
-
+if (isset($_POST["action"]))
+{
+    if ($_POST["action"] == "insert")
+    {
+        $db = Database::connect();
+        $req=$db->prepare('INSERT INTO utilisateur SET nom= ? , mot_de_pass= ? , email= ?,prenom= ? , phone=?');
+        $password=password_hash($password,PASSWORD_BCRYPT);
+        $req->execute([$nom ,$password,  $email, $prenom, $contact,]);
+    }
+}
 
 ?>
     
