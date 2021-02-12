a683cb7dc4317800a8bced4e0b3e62559072ca02
gbelecedric <cedric.gbele@uvci.edu.ci>
2021-02-12 13:23:51 +0000

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,5 +1,18 @@
 <?php>
 
-include(data)
+include('database.php');
+$query = "SELECT * FROM tb_texte";
+$db = Database::connect();
+$statement=$db->prepare($query);
+$statement->execute();
+$result = $statement->fetchAll();
+$total_row = $statement->rowCount();
+
+$output = '
+
+
+';
+
+
     
 ?>
