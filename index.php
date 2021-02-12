a4ef69c13f0daf1528ad265f8c98719ce66d671c
gbelecedric <cedric.gbele@uvci.edu.ci>
2021-02-12 11:57:13 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,60 @@
+<html>  
+    <head>  
+        <title>PHP Ajax Crud using JQuery UI Dialog Box</title>  
+		<link rel="stylesheet" href="jquery-ui.css">
+        <link rel="stylesheet" href="bootstrap.min.css" />
+		<script src="jquery.min.js"></script>  
+		<script src="jquery-ui.js"></script>
+    </head>  
+    <body>  
+        <div class="container">
+			<br />
+			
+			<h3 align="center">PHP Ajax Crud using JQuery UI Dialog</a></h3><br />
+			<br />
+			<div align="right" style="margin-bottom:5px;">
+			<button type="button" name="add" id="add" class="btn btn-success btn-xs">Add</button>
+			</div>
+			<div class="table-responsive" id="user_data">
+				
+			</div>
+			<br />
+		</div>
+		
+		<div id="user_dialog" title="Add Data">
+			<form method="post" id="user_form">
+				<div class="form-group">
+					<label>Enter First Name</label>
+					<input type="text" name="first_name" id="first_name" class="form-control" />
+					<span id="error_first_name" class="text-danger"></span>
+				</div>
+				<div class="form-group">
+					<label>Enter Last Name</label>
+					<input type="text" name="last_name" id="last_name" class="form-control" />
+					<span id="error_last_name" class="text-danger"></span>
+				</div>
+				<div class="form-group">
+					<input type="hidden" name="action" id="action" value="insert" />
+					<input type="hidden" name="hidden_id" id="hidden_id" />
+					<input type="submit" name="form_action" id="form_action" class="btn btn-info" value="Insert" />
+				</div>
+			</form>
+		</div>
+		
+		<div id="action_alert" title="Action">
+			
+		</div>
+		
+		<div id="delete_confirmation" title="Confirmation">
+		<p>Are you sure you want to Delete this data?</p>
+		</div>
+		
+    </body>  
+</html>  
+
+
+
+
+<script>  
+
+</script>
